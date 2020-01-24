18aecfed437c5daf219776c7d2a2049bf3671820
20171cse0610 <60249701+20171cse0610@users.noreply.github.com>
2020-01-24 10:53:55 +0530

    Add files via upload

---
+++ sai11.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{ 
+    char ch;
+    printf("enter the character\n");
+    scanf("%c",&ch);
+    if(ch=='a'||ch=='A')
+    printf("vowel\n");
+    else if(ch=='e'||ch=='E')
+    printf("vowel\n");
+    else if(ch=='i'||ch=='I')
+    printf("vowel\n");
+    else if(ch=='o'||ch=='O')
+    printf("vowel\n");
+    else if(ch=='u'||ch=='U')
+    printf("vowel\n");
+    else
+    printf("consonent\n");
+} 

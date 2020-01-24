7cb157ee6780bf09f2524127dac2ece0ee0977fe
20171cse0610 <60249701+20171cse0610@users.noreply.github.com>
2020-01-24 13:25:50 +0530

    Update sai11.c

--- sai11.c
+++ sai11.c
@@ -7,9 +7,6 @@ void main()
     if(ch=='a'||ch=='A')
     printf("vowel\n");
     else if(ch=='e'||ch=='E')
-    printf("vowel\n");
-    else if(ch=='i'||ch=='I')
-    printf("vowel\n");
     else if(ch=='o'||ch=='O')
     printf("vowel\n");
     else if(ch=='u'||ch=='U')
